51a34dbe8fc9030c5303a176c457b245a8ca1fd0
Snip Snap Release <actions@users.noreply.github.com>
2026-09-03 13:58:10 +0000

    Publish Snip Snap 0.5.0 beta 30 cask

--- Casks/snip-snap@beta.rb
+++ Casks/snip-snap@beta.rb
@@ -1,8 +1,8 @@
 cask "snip-snap@beta" do
-  version "0.5.0-beta.29"
-  sha256 "10461a50d9374a51f73eed3199eaa6624a481513ee16f34971de5198794c0ce2"
+  version "0.5.0-beta.30"
+  sha256 "568317b877b18f2a160369ddaa416cbab38360d141f1e4c1b7090a75724fa7a3"
 
-  url "https://github.com/sreejithraman/snip-snap/releases/download/v0.5.0-beta.29/Snip-Snap-0.5.0.zip"
+  url "https://github.com/sreejithraman/snip-snap/releases/download/v0.5.0-beta.30/Snip-Snap-0.5.0.zip"
   name "Snip Snap Beta"
   desc "Keep saved snips ready to use later"
   homepage "https://sree.world/snip-snap"
