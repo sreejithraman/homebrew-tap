aacc6b08c7eb4d9a517319ed6905d3bc0febad3c
Snip Snap Release <actions@users.noreply.github.com>
2026-09-11 04:28:26 +0000

    Publish Snip Snap 0.5.0 cask

--- Casks/snip-snap.rb
+++ Casks/snip-snap.rb
@@ -1,10 +1,10 @@
 cask "snip-snap" do
-  version "0.4.1"
-  sha256 "911bf309584f2f1a15d116354db5769a27d89b7d616dfccf1951b72c18d8fa24"
+  version "0.5.0"
+  sha256 "804da8952e7ab4c853e05a67082c351bed8241abca44308087d9c853621dcbc9"
 
   url "https://github.com/sreejithraman/snip-snap/releases/download/v#{version}/Snip-Snap-#{version}.zip"
   name "Snip Snap"
-  desc "Keep clips ready to use later"
+  desc "Keep saved snips ready to use later"
   homepage "https://sree.world/snip-snap"
 
   auto_updates true
